e63f3dc16a66c42067bafa341c1313bb22c13e81
gabrieleHP <tibonigabriele@gmail.com>
2022-11-15 23:56:27 +0100

    cleaning imports

--- dropo/dropo.py
+++ dropo/dropo.py
@@ -5,9 +5,6 @@ from copy import deepcopy
 from multiprocessing import Pool
 
 import numpy as np
-import matplotlib.pyplot as plt
-import matplotlib
-import pandas as pd
 from sklearn.preprocessing import StandardScaler
 from scipy.stats import multivariate_normal
 from scipy.stats import truncnorm
