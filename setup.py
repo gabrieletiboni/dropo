358263b6ebd7f85d256324336074c91a87b43f73
gtiboni@watt <gtiboni@watt>
2023-08-03 11:42:10 +0200

    required gym version is 0.21.0

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='dropo',
-      version='2.0',
+      version='2.1',
       install_requires=['numpy','nevergrad'],  # And any other dependencies foo needs
       packages=find_packages(exclude=["datasets"])
 )
